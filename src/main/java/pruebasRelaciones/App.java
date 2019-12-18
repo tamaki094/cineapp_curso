65e7e51db4dbe8ca1902a795b76767cb704e1351
Edri Alan Angulo Arteaga <edri.alan.94@gmail.com>
2019-12-17 22:28:22 -0600

    -Se añadieron el resto de repositorios para todas las clases de modelo -Aprendi a  hacer relaciones 1a1 1aM y a la inversa -Volvimos a mover el archivo xml root donde estan los BEANS a la parte de los resoruce de cineapp

--- src/main/java/pruebasRelaciones/App.java
+++ src/main/java/pruebasRelaciones/App.java
@@ -1,18 +1,22 @@
 package pruebasRelaciones;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import net.itinajero.app.model.Detalle;
+import net.itinajero.app.model.Horario;
 import net.itinajero.app.model.Pelicula;
 import net.itinajero.app.repository.DetallesRepository;
+import net.itinajero.app.repository.HorariosRepository;
 import net.itinajero.app.repository.PeliculasRepository;
 
 public class App {
 
 	public static void main(String[] args) 
 	{
+		//Ejemplo 1--------------------------------------------------------------------------------------------------------
 //		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("root-context.xml");
 //		//Recuperar todas las entidades de tipo pelicula
 //		
@@ -27,19 +31,46 @@ public class App {
 //		
 //		context .close();
 		
+		//Ejemplo 2--------------------------------------------------------------------------------------------------------
+		
+//		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("root-context.xml");
+//		
+//		DetallesRepository repo = context.getBean("detallesRepository", DetallesRepository.class);
+//		
+//		List<Detalle> lista =repo.findAll();
+//		
+//		for (Detalle detalle : lista) 
+//		{
+//			System.out.println(detalle);
+//		}
+//		
+//		context .close();
+		
+		//Ejemplo 3--------------------------------------------------------------------------------------------------------
+//		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("root-context.xml");
+//		
+//		HorariosRepository repo = context.getBean("horariosRepository", HorariosRepository.class);
+//		
+//		List<Horario> lista = repo.findAll();
+//		
+//		for (Horario horario : lista) {
+//			System.out.println(horario);
+//		}
+//		
+//		context .close();
+		
+		//Ejemplo 4--------------------------------------------------------------------------------------------------------
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("root-context.xml");
 		
-		DetallesRepository repo = context.getBean("detallesRepository", DetallesRepository.class);
+		PeliculasRepository repo = context.getBean("peliculasRepository", PeliculasRepository.class);
 		
-		List<Detalle> lista =repo.findAll();
 		
-		for (Detalle detalle : lista) 
-		{
-			System.out.println(detalle);
-		}
+		Optional<Pelicula> optional = repo.findById(7);
+		
+		 System.out.println(optional.get().getHorarios().size() + " horarios para la pelicula.");
+		 System.out.println(optional.get());
 		
 		context .close();
-
 
 	}
 
